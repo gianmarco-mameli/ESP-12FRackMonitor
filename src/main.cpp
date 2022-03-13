3bbc66079271869e3fbade9bb22b9d30eff96cfc
Gianmarco Mameli <57061995+gianmarco-mameli@users.noreply.github.com>
2022-03-13 23:58:07 +0100

    ultime modifiche

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,16 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "DHT.h"
+#include "secrets.h"
+
 #define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+float t = 0;
+float h = 0;
+
+#define PIRPIN 14
+int pir = 0;
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -41,31 +48,29 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
      B01110000, B01110000,
      B00000000, B00110000};
 
-const char *ssid = "WLAN";                     // Enter your WIFI ssid
-const char *password = "1qazXSW23edcVFR45tgb"; // Enter your WIFI password
-
-const char *mqtt_broker = "192.168.1.1";
-const int mqtt_port = 1883;
-const char *base_topic = "rpi/"
-const char *temp = base_topic + "temperature"
-const char *hum = base_topic + "humidity"
-const char *motion = base_topic + "motion"
+const char *temp = "rackmonitor/temperature";
+const char *hum = "rackmonitor/humidity";
+const char *motion = "rackmonitor/motion";
 
 WiFiClient espClient;
-PubSubClient client(espClient);
+String ip;
 
-void notehum(float hum)
-{
-  display.drawRect(1, 1, display.width() - 1, display.height() - 1, WHITE);
-  display.setTextSize(2);
-  display.setCursor(15, 10);
-  display.print(hum);
-  display.setCursor(80, 10);
-  display.print("%");
-  display.setCursor(112, 10);
-  display.print("H");
-  display.display();
-}
+PubSubClient client(espClient);
+const char *client_id = "rackmonitor";
+
+// void notehum(float hum)
+// {
+//   display.drawRect(1, 1, display.width() - 1, display.height() - 1, WHITE);
+//   display.setTextSize(2);
+//   display.setCursor(15, 10);
+//   display.print(hum);
+//   display.setCursor(80, 10);
+//   display.print("%");
+//   display.setCursor(112, 10);
+//   display.print("H");
+//   display.display();
+// }
+
 
 void notetemp(float temp, char C_F)
 {
@@ -77,87 +82,128 @@ void notetemp(float temp, char C_F)
   display.setTextSize(2);
   display.setCursor(112, 10);
   display.print(C_F);
-
-  // if (temp >= 100 || temp < 0)
-  // {
-  //   display.setTextSize(2);
-  //   display.setCursor(15, 10);
-  // }
-  // else if (temp < 10 && temp >= 0)
-  // {
   display.setTextSize(3);
-  display.setCursor(25, 6);
-  // }
-
-  // else
-  // {
-  //   display.setTextSize(3);
-  //   display.setCursor(10, 6);
-  // }
-
+  display.setCursor(10, 6);
   display.print(temp);
   display.display();
 }
 
-void Pir(void)
-{
-  int pir = digitalRead(14);
 
-  if (pir == HIGH)
-  {
-    display.setTextSize(1);
-    display.setCursor(25, 11);
-    display.print("MOTION DETECTED");
-    display.display();
-  }
-  else if (pir == LOW)
-  {
-    display.setTextSize(1);
-    display.setCursor(25, 11);
-    display.print("NO MOTION ");
-    display.display();
-  }
+int Pir(void)
+{
+  int pir = digitalRead(PIRPIN);
+
+  char *message = "";
+
+  itoa(pir,message,10);
+
+  client.publish(motion,message);
+
+  // Serial.println(message);
+
+  return pir;
 }
+void getDht(void) {
+  // delay(2000); // Wait a few seconds for sensor to measure
 
-void showTemp(void)
-{
-  delay(2000); // Wait a few seconds for sensor to measure
+  h = dht.readHumidity();
+  t = dht.readTemperature();
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-
-  if (isnan(h) || isnan(t) || isnan(f))
+  if (isnan(h) || isnan(t) )
   {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+  char *message = "";
 
-  // notehum(h);
-  // display.clearDisplay();
-  // delay(3000);
-  notetemp(t, 'C');
+  dtostrf(t,4,2,message);
+  client.publish(temp,message);
+  itoa(h,message,10);
+  client.publish(hum,message);
 
-  // delay(3000);
-  // display.clearDisplay();
-  // notetemp(f, 'F');
-  // delay(3000);
 }
+
+void showInfo(void)
+{
+  display.drawRect(1, 1, display.width() - 1, display.height() - 1, WHITE);
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(102, 5);
+  display.print("o");
+  display.setTextSize(2);
+  display.setCursor(110, 12);
+  display.print("C");
+  display.setTextSize(3);
+  display.setCursor(8, 8);
+  display.print(t);
+
+  display.setTextSize(1);
+  display.setCursor(8, 50);
+  display.print(WiFi.localIP());
+
+  display.display();
+}
+
 
 void connectWifi(void)
 {
+  display.drawRect(1, 1, display.width() - 1, display.height() - 1, WHITE);
+  display.setTextSize(2);
+  display.setCursor(15, 10);
+  display.print("Connecting to Wifi");
+  // display.display();
   WiFi.begin(ssid, password);
   if (WiFi.waitForConnectResult() == WL_CONNECTED)
   {
-    Serial.print("Connected. IP: ");
-    Serial.println(WiFi.localIP());
+    // Serial.print("Connected. IP: ");
+    // Serial.println(WiFi.localIP());
+    display.setCursor(15, 20);
+    display.print("Connected. IP: ");
+    display.println(WiFi.localIP());
+    // display.display();
   }
   else
   {
     Serial.println("Connection Failed!");
   }
-  Serial.println("Connected to the WiFi network");
+  // Serial.println("Connected to the WiFi network");
+  display.setCursor(15, 30);
+  display.println("Connected to the WiFi");
+  // display.display();
 }
+
+void callback(char *topic, byte *payload, unsigned int length) {
+//   Serial.print("Message arrived in topic: ");
+
+//   for (int i = 0; i < length; i++) {
+//     message = message + (char) payload[i];  // convert *byte to string
+//   }
+//   Serial.println(topic);
+//   Serial.println(message);
+
+//   // if (strcmp(topic,temperature)==0) {
+//   //   temp = message;
+//   // }
+//   // if (strcmp(topic,pump1)==0) {
+//   //   p1 = message;
+//   // }
+
+}
+
+void InitMqtt() {
+    //connecting to a mqtt broker
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    Serial.println("Connecting to MQTT broker.....");
+    if (client.connect(client_id)) {
+      Serial.println("MQTT broker connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+}
+
 
 // #if (SSD1306_LCDHEIGHT != 64)
 // #error("Height incorrect, please fix Adafruit_SSD1306.h!");
@@ -166,16 +212,6 @@ void setup()
 {
   Serial.begin(9600);
 
-  connectWifi();
-
-  // connecting to a mqtt broker
-  client.setServer(mqtt_broker, mqtt_port);
-
-  pinMode(14, INPUT);
-  Serial.begin(9600);
-  Serial.println("DHT11 Monitor System");
-  dht.begin();
-
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("SSD1306 allocation failed");
@@ -183,8 +219,18 @@ void setup()
   }
 
   display.display();
-  // delay(2000);
-  // display.clearDisplay();
+  delay(2000);
+  display.clearDisplay();
+
+  connectWifi();
+  InitMqtt();
+
+  pinMode(PIRPIN, INPUT);
+
+  // Serial.println("DHT11 Monitor System");
+  dht.begin();
+
+
   // display.drawPixel(10, 10, WHITE);
   // display.display();
   // delay(2000);
@@ -207,8 +253,22 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  showTemp();
-  // display.clearDisplay();
-  Pir();
+  getDht();
+  int pir = Pir();
+  delay(1000);
+  // client.loop();
+  // Serial.println(pir);
+  if (pir == 1)
+  {
+    showInfo();
+    display.clearDisplay();
+  }
+  else if (pir == 0)
+  {
+    display.display();
+    display.clearDisplay();
+  }
+
+
   // display.clearDisplay();
 }
